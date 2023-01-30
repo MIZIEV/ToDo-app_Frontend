59b4cfa59d66883e09c23d5088281b9209f61406
Alexandr Mizev <mizzzev@outlook.com>
2023-01-30 22:28:03 +0200

    added logic in (Todo & TodoList) components, also change style in App.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
+import TodoForm from './components/Todos/TodoForm';
+import TodoList from './components/Todos/TodoList';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState(['todo one', 'todo two', 'todo three']);
+
   return (
     <div className="App">
+      <h1>Todo app</h1>
+      <TodoForm />
+      <TodoList todos={todos} />
 
     </div>
   );
---
+++ src/components/Todos/Todo.js
@@ -0,0 +1,7 @@
+function Todo(props) {
+    const { todo } = props
+
+    return <h1>{todo}</h1>
+}
+
+export default Todo;
