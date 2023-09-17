8022a1af790546f47b66ce12e082c3d489567378
Miziev <mizzzev@outlook.com>
2023-09-17 14:26:29 +0300

    created primitive register form

--- src/components/auth/RegisterComponent.jsx
+++ src/components/auth/RegisterComponent.jsx
@@ -1,9 +1,71 @@
 import React, { useState } from "react";
 
 function RegisterComponent() {
+
+    const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+
+
+
+
+
     return (
         <div>
             <h1>Register Component</h1>
+
+            <h3>Registe form</h3>
+            <form>
+
+                <div>
+                    <label>Name</label>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Enter your name"
+                        value={name}
+                        onChange={(e) => { setName(e.target.value) }}
+                    ></input>
+                </div>
+
+                <div>
+                    <label>Username</label>
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="Enter your username"
+                        value={username}
+                        onChange={(e) => { setUsername(e.target.value) }}
+                    ></input>
+                </div>
+
+                <div>
+                    <label>Email</label>
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="Enter your email"
+                        value={email}
+                        onChange={(e) => { setEmail(e.target.value) }}
+                    ></input>
+                </div>
+
+                <div>
+
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={(e) => { setPassword(e.target.value) }}
+                    ></input>
+                </div>
+
+            </form>
         </div>
     )
 }
