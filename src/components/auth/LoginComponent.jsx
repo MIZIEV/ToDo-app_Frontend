afc1b5000114cdba0c01029724decb391f2d4f8a
Miziev <mizzzev@outlook.com>
2023-09-17 15:27:50 +0300

    created four functions in auth service for login logic & implement it in login component

--- src/components/auth/LoginComponent.jsx
+++ src/components/auth/LoginComponent.jsx
@@ -1,9 +1,30 @@
 import React, { useState } from "react";
+import { loginApiCall, saveLoggedInUser, storeToken } from "../../services/AuthService";
 
 function LoginComponent() {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+
+    async function handleLoginForm(e) {
+        e.preventDefault();
+        const loginObj = [username, password];
+        console.log(loginObj);
+
+        await loginApiCall(username, password).
+            then((responce) => {
+                console.log(responce.data)
+
+                const token = "Bearer " + responce.data.accessToken;
+                const role = responce.data.role;
+
+                storeToken(token);
+
+                saveLoggedInUser(username, role);
+                window.location.reload(false);
+            }).catch(error => console.error(error));
+
+    }
 
 
     return (
@@ -36,7 +57,7 @@ function LoginComponent() {
                 </div>
 
                 <div>
-                    <button type="button">Submit</button>
+                    <button type="button" onClick={(e) => handleLoginForm(e)}>Submit</button>
                 </div>
             </form>
         </div>
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -3,3 +3,13 @@ import axios from "axios";
 const AUTH_REST_API_BASE_URL = "http://localhost:8080/api/auth";
 
 export const registerApiCall = (registerObj) => axios.post(AUTH_REST_API_BASE_URL + "/register", registerObj);
+
+export const loginApiCall = (usernameOrEmail, password) => axios.post(AUTH_REST_API_BASE_URL + "/login", { usernameOrEmail, password });
+
+export const storeToken = (token) => localStorage.setItem("token", token);
+export const getToken = () => localStorage.getItem("token");
+
+export const saveLoggedInUser = (username, role) => {
+    sessionStorage.setItem("authenticatedUser", username);
+    sessionStorage.setItem("role", role);
+}
