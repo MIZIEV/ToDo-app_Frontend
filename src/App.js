cae7b19536d2655c6b4997ece293396a09946bae
Miziev <mizzzev@outlook.com>
2023-09-18 18:51:57 +0300

    - removed unnecessary code in main App file - added UseNavigate in LoginComponent - changed TodoList (also removed unnacessary code & prepared for new logic)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import { useEffect, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-
-import TodoForm from './components/Todos/TodoForm';
 import TodoList from './components/Todos/TodoList';
-import TodosActions from './components/Todos/TodosAcions';
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
@@ -12,95 +7,19 @@ import LoginComponent from './components/auth/LoginComponent';
 import RegisterComponent from './components/auth/RegisterComponent';
 import HeaderComponent from './components/UI/HeaderComponent';
 import FooterComponent from './components/UI/FooterComponent';
-
-const URL_PUT = "http://localhost:8080/api/add";
-const URL_GET = "http://localhost:8080/api/todos";
-const URL_UPDATE = "http://localhost:8080/api/todo/";
-const URL_DELETE_ALL = "http://localhost:8080/api/todos/delete";
-const URL_DELETE_COMPLETED_TODOS = "http://localhost:8080/api/todos/delete_completed";
+import { isUserLoggedIn } from './services/AuthService';
 
 function App() {
 
-  const [todos, setTodos] = useState([]);
+  function AuthenticateRoute({ children }) {
+    const isAuth = isUserLoggedIn();
 
-  const addTodoHandler = (text) => {
-
-    const newTodo = {
-      text: text,
-      completed: false,
-      todoUniqueKey: uuidv4()
+    if (isAuth) {
+      return children;
+    } else {
+      <Navigate to="/" />
     }
-
-    setTodos([...todos, newTodo]);
-
-    fetch(URL_PUT, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newTodo)
-    })
   }
-
-  /*useEffect(() => {
-    fetch(URL_GET)
-      .then(response => response.json())
-      .then(result => { setTodos(result) })
-  }, [])*/
-
-  const deleteTodoHandler = (todoUniqueKey) => {
-    setTodos(todos.filter((todo) => todo.todoUniqueKey !== todoUniqueKey))
-
-    fetch(URL_UPDATE + todoUniqueKey, {
-      method: "DELETE",
-    })
-
-  }
-
-  const toggleTodoHandler = (todoUniqueKey) => {
-
-    setTodos(todos.map((todo) => {
-      return todo.todoUniqueKey === todoUniqueKey ? { ...todo, completed: !todo.completed } : { ...todo }
-    }))
-
-    todos.map((todo) => {
-      if (todo.todoUniqueKey === todoUniqueKey) {
-
-        const updatedTodo = {
-          ...todo,
-          completed: !todo.completed
-        }
-
-        fetch(URL_UPDATE + todoUniqueKey, {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(updatedTodo)
-        })
-      }
-    })
-  }
-
-  const resetTodosHandler = () => {
-
-    fetch(URL_DELETE_ALL, {
-      method: "DELETE",
-    })
-    setTodos([])
-  }
-
-  const deleteCompletedTodosHandler = () => {
-
-    fetch(URL_DELETE_COMPLETED_TODOS, {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(todos.filter((todo) => todo.completed))
-    })
-
-    setTodos(todos.filter((todo) => !todo.completed))
-  }
-
-  const completedTodosCount = todos.filter((todo) => todo.completed).length
-
-
-
 
   return (
 
@@ -110,36 +29,24 @@ function App() {
         <HeaderComponent />
 
         <Routes>
+          <Route path='/' element={<LoginComponent />} />
+
+
+          <Route path='/todos' element={
+            <AuthenticateRoute>
+              <TodoList />
+            </AuthenticateRoute>
+          } />
+
           <Route path='/login' element={<LoginComponent />} />
           <Route path="/register" element={<RegisterComponent />} />
+
         </Routes>
 
         <FooterComponent />
       </BrowserRouter>
 
     </div>
-
-    /*
-        { old logic, I'll keep it for now 
-    
-          <div className="App">
-          <h1>Todo app</h1>
-          <TodoForm addTodo={addTodoHandler} />
-    
-          {todos.length > 0 && <TodosActions
-            completedTodosExists={!!completedTodosCount}
-            resetTodos={resetTodosHandler}
-            deleteCompletedTodos={deleteCompletedTodosHandler} />}
-    
-          <TodoList todos={todos} deleteTodo={deleteTodoHandler} toggleTodo={toggleTodoHandler} />
-    
-          {completedTodosCount > 0 && (
-            <h3>{`You have completed ${completedTodosCount} ${completedTodosCount > 1 ? "todos" : "todo"}`}</h3>
-          )}
-        </div>}
-    */
-
-
   );
 }
 
--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,13 +1,30 @@
 import TodoComponent from "./TodoComponent";
 import styles from '../../styles/TodoList.module.css'
+import { getAllTodos } from "../../services/TodoService";
+import { useEffect, useState } from "react";
 
-function TodoList(props) {
+function TodoList() {
 
-    const { todos, deleteTodo, toggleTodo } = props
+    //const { todos, deleteTodo, toggleTodo } = props
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        listTodos();
+    }, [])
+
+
+    function listTodos() {
+        getAllTodos().then((responce) => {
+            console.log(responce.data);
+            setTodos(responce.data);
+        }).catch(error => console.error(error));
+    }
+
+
     return (
         <div className={styles.todoListContainer}>
-            {todos.length === 0 && <h3>Todo list is empty.</h3>}
-            {todos.map((todo) => <TodoComponent key={todo.todoUniqueKey} todo={todo} deleteTodo={deleteTodo} toggleTodo={toggleTodo} />)}
+            {/*todos.length === 0 && <h3>Todo list is empty.</h3>*/}
+            {todos.map((todo) => <TodoComponent key={todo.todoUniqueKey} todo={todo} />)}
         </div>
     )
 }
