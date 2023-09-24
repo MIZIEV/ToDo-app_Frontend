e9b78ac58eaa8d8843b1252a0791062b3a8292aa
Miziev <mizzzev@outlook.com>
2023-09-24 16:07:30 +0300

    added button & nav link for logout and adding new todos (it avalible only after authorization)

--- src/components/UI/HeaderComponent.jsx
+++ src/components/UI/HeaderComponent.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { isUserLoggedIn, logout } from "../../services/AuthService";
 
 function HeaderComponent() {
+
+    const isAuth = isUserLoggedIn();
+    const navigator = useNavigate();
+
+    function addNewTodo() {
+        navigator("/add-todo");
+    }
+
+    function handleLogout() {
+        logout();
+        navigator("/login");
+    }
 
     return (
         <div>
             <h1>HEADER</h1>
+            {
+                isAuth &&
+                <button onClick={addNewTodo}>Add new todo</button>
+            }
+            {
+                isAuth &&
+                <NavLink to="/login" onClick={handleLogout}>Logout</NavLink>
+            }
         </div>
     )
 }
