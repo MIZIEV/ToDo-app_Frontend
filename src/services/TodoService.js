397acf3027bd37195b778737f4c7ed959385bf4c
Miziev <mizzzev@outlook.com>
2023-09-24 20:20:40 +0300

    created two functions in todo service (get one todo by key & update todo)

--- src/services/TodoService.js
+++ src/services/TodoService.js
@@ -13,4 +13,8 @@ axios.interceptors.request.use(function (config) {
 
 export const getAllTodos = () => axios.get(BASE_REST_API_URL + "/todos");
 
+export const getTodoByKey = (todoUniqueKey) => axios.get(BASE_REST_API_URL + "/todo/" + todoUniqueKey);
+
 export const saveTodo = (todo) => axios.post(BASE_REST_API_URL + "/add", todo);
+
+export const updateTodod = (todo, todoUniqueKey) => axios.put(BASE_REST_API_URL + "/" + todoUniqueKey, todo);
