40b6708c48b1035b9e57058c20233cd26449e5fc
Miziev <mizzzev@outlook.com>
2023-09-27 17:03:48 +0300

    started implementing new css styles in header & footer component

--- src/components/UI/HeaderComponent.jsx
+++ src/components/UI/HeaderComponent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { isUserLoggedIn, logout } from "../../services/AuthService";
+import styles from "../../styles/Header.module.css"
 
 function HeaderComponent() {
 
@@ -16,18 +17,36 @@ function HeaderComponent() {
         navigator("/login");
     }
 
+    function registrationHandler() {
+        navigator("/register")
+    }
+
     return (
-        <div>
-            <h1>HEADER</h1>
-            {
-                isAuth &&
-                <button onClick={addNewTodo}>Add new todo</button>
-            }
-            {
-                isAuth &&
-                <NavLink to="/login" onClick={handleLogout}>Logout</NavLink>
-            }
-        </div>
+
+        <header className={`${styles.header}`}>
+            <div className={`${styles.maindiv}`}>
+                <div className={`${styles.divleft}`}>
+                    <h3>Todo manager</h3>
+                </div>
+
+                {
+                    //   isAuth &&
+                    // <button onClick={addNewTodo}>Add new todo</button>
+                }
+                {
+                    !isAuth &&
+                    <div className={`${styles.divright}`}>
+                        <NavLink className={`${styles.navlinksignin}`} to="/login">Sing in</NavLink>
+                        <button onClick={() => registrationHandler()}>Sing up</button>
+                    </div>
+                }
+                {
+                    isAuth &&
+                    <NavLink to="/login" onClick={handleLogout}>Logout</NavLink>
+                }
+            </div>
+        </header>
+
     )
 }
 
