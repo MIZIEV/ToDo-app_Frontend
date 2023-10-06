3f60a499627ec1accfbc2e7e949609196045aa62
Miziev <mizzzev@outlook.com>
2023-10-06 18:15:26 +0300

    improve todo element component (added useState for chackboc, now it changing visual status witheout realoded winwow

--- src/components/Todos/TodoElementComponent.jsx
+++ src/components/Todos/TodoElementComponent.jsx
@@ -8,22 +8,21 @@ import { changeCompleteStatus } from "../../services/TodoElementService";
 function TodoElementComponent(props) {
 
     const { todoElement } = props;
+    const [checked, setChecked] = useState(todoElement.completed);
 
-    //const [elementName, setElementName] = useState("");
-    //const [completed, setCompleted] = useState(false);
+    function changeStatusHandler(event) {
+        setChecked(event.target.checked);
 
-    function changeStatusHandler() {
         changeCompleteStatus(todoElement.id).then((responce) => {
             console.log(responce.data);
         }).catch(error => console.error(error));
-
     }
-
 
     return (
         <div>
             <FormGroup>
-                <FormControlLabel control={<Checkbox checked={todoElement.completed} onChange={changeStatusHandler} />} label={todoElement.elementName} />
+                <FormControlLabel control={<Checkbox checked={checked}
+                    onChange={changeStatusHandler} />} label={todoElement.elementName} />
             </FormGroup>
         </div>
     )
