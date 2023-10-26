b158149ddf5765e0b79a85ec5a1b6f1523c9e5b5
Miziev <mizzzev@outlook.com>
2023-10-26 15:56:38 +0300

    created new module css for element component with styles for delete button, added functional for deleting elements in service & implemented it in element component

--- src/components/Todos/TodoElementComponent.jsx
+++ src/components/Todos/TodoElementComponent.jsx
@@ -3,8 +3,11 @@ import React, { useState } from "react";
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import { changeCompleteStatus } from "../../services/TodoElementService";
+import { changeCompleteStatus, deleteElement } from "../../services/TodoElementService";
 import sound from "../../sounds/check_box_sound.mp3";
+import { BsXSquareFill } from "react-icons/bs";
+import styles from "../../styles/Element.module.css";
+
 
 function TodoElementComponent(props) {
 
@@ -18,7 +21,7 @@ function TodoElementComponent(props) {
         changeCompleteStatus(todoElement.id).then((responce) => {
             console.log(responce.data);
 
-             window.location.reload();
+            window.location.reload();
             //todo: it is a temporary solution (window.location.reload()) must be removed
 
         }).catch(error => console.error(error));
@@ -28,9 +31,15 @@ function TodoElementComponent(props) {
         new Audio(sound).play();
     }
 
+    function removeElementHandler(id) {
+        deleteElement(id).then((response) => {
+            console.log(response.data);
+        }).catch(error => console.error(error));
+    }
+
     return (
         <div>
-            <FormGroup>
+            <FormGroup className={styles.formGrup}>
                 <FormControlLabel control={<Checkbox
                     sx={{
                         color: "white",
@@ -39,8 +48,11 @@ function TodoElementComponent(props) {
                         }
                     }}
                     checked={checked}
-                    onChange={(e) => changeStatusHandler(e)} />} label={todoElement.elementName} />
+                    onChange={(e) => changeStatusHandler(e)} />}
+                    label={<span>{todoElement.elementName}
+                        <button className={styles.deleteButton} onClick={() => removeElementHandler(todoElement.id)}><BsXSquareFill className={styles.deleteIcon} /></button></span>} />
             </FormGroup>
+
         </div>
     )
 }
--- src/services/TodoElementService.js
+++ src/services/TodoElementService.js
@@ -16,3 +16,5 @@ export const saveNewTodoElement = (todoUniqueKey, todoElement) =>
 export const getAllElements = (todoUniqueKey) => axios.get(BASE_REST_API_URL + "/element/list/" + todoUniqueKey);
 
 export const changeCompleteStatus = (id) => axios.patch(BASE_REST_API_URL + "/element/change-status/" + id);
+
+export const deleteElement = (id) => axios.delete(BASE_REST_API_URL + "/element/remove/" + id);
